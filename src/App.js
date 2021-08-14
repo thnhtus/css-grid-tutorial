644b2fa75d8fb8b122644eea06633d0ec61f0c4a
tubt4 <36790617+thnhtus@users.noreply.github.com>
2021-08-14 23:33:45 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="layout">
+        <div className="layout-item">
+          <img
+            src="https://images.unsplash.com/photo-1500835556837-99ac94a94552?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+            alt=""
+            className="layout-image"
+          />
+        </div>
+        <div className="layout-item">
+          <img
+            src="https://images.unsplash.com/photo-1508672019048-805c876b67e2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1093&q=80"
+            alt=""
+            className="layout-image"
+          />
+        </div>
+        <div className="layout-item">
+          <img
+            src="https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+            alt=""
+            className="layout-image"
+          />
+        </div>
+        <div className="layout-item">
+          <img
+            src="https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+            alt=""
+            className="layout-image"
+          />
+        </div>
+        <div className="layout-item">
+          <img
+            src="https://images.unsplash.com/photo-1523906834658-6e24ef2386f9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=630&q=80"
+            alt=""
+            className="layout-image"
+          />
+        </div>
+        <div className="layout-item">
+          <img
+            src="https://images.unsplash.com/photo-1476900543704-4312b78632f8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+            alt=""
+            className="layout-image"
+          />
+        </div>
+      </div>
     </div>
   );
 }
